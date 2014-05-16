8b1b26d69172db85b263713f15bb719b76991697
isralimas <tiolimas23@yahoo.com.mx>
2014-05-16 14:02:45 -0500

    suma html

--- project/app/views.py
+++ project/app/views.py
@@ -1,3 +1,29 @@
+# -*- encoding: utf-8 -*-
 from django.shortcuts import render
+from django.http import HttpResponse
+import datetime
 
-# Create your views here.
+def suma(request,opera,num1,num2):
+	if opera == 'suma':
+		now = int(num1) + int(num2)
+	elif opera == 'resta' :
+		now = int(num1) - int(num2)
+	elif opera == 'multiplica' :
+		now = int(num1) * int(num2)
+	elif opera == 'divide' :
+		try :
+			now = int(num1) / int(num2)
+		except Exception, e :
+			now = str(e)
+	else :
+		now = 'No entendí que clase de operacion quieres hacer'
+		#now = datetime.datetime.now()
+    #now = datetime.datetime.now()
+	html = "<html><body>Isras, el resultado de tu operación es %s.</body></html>" % now
+	return HttpResponse(html)
+
+def current_datetime(request,num1,num2):
+    #now = int(num1) + int(num2)
+    now = datetime.datetime.now()
+    html = "<html><body>La hora que marca el reloj es %s.</body></html>" % now
+    return HttpResponse(html)
--- project/project/urls.py
+++ project/project/urls.py
@@ -1,3 +1,4 @@
+# -*- encoding: utf-8 -*-
 from django.conf.urls import patterns, include, url
 
 from django.contrib import admin
@@ -5,8 +6,12 @@ admin.autodiscover()
 
 urlpatterns = patterns('',
     # Examples:
-    # url(r'^$', 'project.views.home', name='home'),
+    #url(r'^isra/([A-z]{4})/([A-z]{2})/$', 'app.views.current_datetime'),
+
+    url(r'^([A-z]+)/(.+)/(.+)/$', 'app.views.suma', name='suma'),
+
     # url(r'^blog/', include('blog.urls')),
 
-    url(r'^admin/', include(admin.site.urls)),
+    #url(r'^admin/', include(admin.site.urls)),
+    url(r'^administrador/', include(admin.site.urls)),
 )
